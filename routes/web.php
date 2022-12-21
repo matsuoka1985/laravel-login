c804b1fae975ed599801577c6ef763889fba7f75
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2022-12-21 16:11:34 +0900

    ログインフォーム作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use
+App\Http\Controllers\Auth\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/",[AuthController::class,"showLogin"])->name("showLogin");
+
+Route::post("login",[AuthController::class,"login"])->name("login");
