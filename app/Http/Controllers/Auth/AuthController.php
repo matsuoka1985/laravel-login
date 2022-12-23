3f4ef0bac4462942b2d0c22eb244d48511a5a9be
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2022-12-23 21:17:00 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;#
 
 
 class AuthController extends Controller
@@ -23,13 +24,46 @@ class AuthController extends Controller
     public function login(LoginFormRequest $request){
         // dd($request->all());
         $credentials=$request->only("email","password");
-        //ログインが成功していたら
-        if(Auth::attempt($credentials)){
-            //sessionを再生成。
-            $request->session()->regenerate();
 
-            return redirect()->route('home')->with("success","ログイン成功しました");
+        //①アカウントがロックされていたら弾く。
+        $user=User::where('email','=',$credentials['email'])->first();
+        //送信されたemailを持つuserが存在した場合
+        if(!is_null($user)){
+            //userがアカウントロックされていた場合
+            if($user->locked_flg===1){
+                return back()->withErrors([
+                    "danger"=>"アカウントがロックされています"
+                ]);
+            }
+            //送信されたemailを持つuserが存在し、かつuserがアカウントロックされてない場合
+            //ログインが成功していたら
+            if(Auth::attempt($credentials)){
+                //sessionを再生成。
+                $request->session()->regenerate();
+                // ②成功したらエラーカウントを0に戻す。
+                if($user->error_count > 0){
+                    $user->error_count=0;
+                    $user->save();
+                }
+                return redirect()->route('home')->with("success","ログイン成功しました");
+            }
+            //③ログイン失敗したらエラーカウントを1増やす。
+            $user->error_count=$user->error_count+1;
+            //④エラーカウントが6以上の場合はアカウントをロックする。
+            if($user->error_count>5){
+                $user->locked_flg=1;
+                $user->save();
+
+                return back()->withErrors([
+                    "danger"=>"アカウントがロックされました。解除したい場合は運営者に連絡してください。"
+                ]);
+            }
+
+            $user->save();
         }
+
+
+
         //ログインが失敗した場合は元の画面に戻ってもらう。
         //エラー内容と共に元の画面に返すことができる。
         //withErrorsの中身はsessionで返すことができる。
